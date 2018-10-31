71607fe6444ebfde142428039e23f96e94e41636
LNBertolini <lunbertolini@gmail.com>
2018-10-31 16:26:40 -0500

    more pass stuff

--- knexfile.js
+++ knexfile.js
@@ -4,15 +4,20 @@ const pg = require('pg')
 /*
 knex setup for access to localhost and production version of the repo
 */
+let config = {};
+try {
+  config = require('../config.js');
+} catch (err) {
+}
 module.exports = {
 
   development: {
     client: 'pg',
     connection: {
-      host: 'localhost',
-      user: 'rentlurpg',
-      password: 'rentlurpgpass',
-      database: 'rentlur',
+       host:  "localhost",
+       user: config.DB_USER || "postgres",
+       password: config.DB_PASS || '',
+       database: "rentlur"
     },
   },
   production: {
